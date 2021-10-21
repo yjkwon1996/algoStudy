e9b0b66c6d0c8e5e03c76e0c298b9e68226c3525
yjkwon1996 <yjkwon1996@gmail.com>
2021-10-21 18:01:57 +0900

    오픈채팅방

---
+++ openchat.py
@@ -0,0 +1,16 @@
+# 2019 KAKAO BLIND RECRUITMENT 오픈채팅방
+# https://programmers.co.kr/learn/courses/30/lessons/42888
+def solution(record):
+    answer = []
+    
+    
+    
+    
+    
+    return answer
+
+if __name__ == "__main__" :
+   record = ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+   print(solution(record)) 
+   # ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+   
