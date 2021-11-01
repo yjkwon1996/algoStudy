2063547b5a030e606e2cb446a6416c26d599a582
yjkwon1996 <yjkwon1996@gmail.com>
2021-11-01 19:25:29 +0900

    괄호 변환

---
+++ bracket_conversion.py
@@ -0,0 +1,17 @@
+# 2020 KAKAO BLIND RECRUITMENT
+# https://programmers.co.kr/learn/courses/30/lessons/60058
+
+
+def solution(p):
+    answer = ''
+    return answer
+
+
+
+if __name__ == "__main__" :
+    p1 = "(()())()" # "(()())()"
+    p2 = ")(" # "()"
+    p3 = "()))((()" # "()(())()"
+    print(solution(p1))
+    print(solution(p2))
+    print(solution(p3))
