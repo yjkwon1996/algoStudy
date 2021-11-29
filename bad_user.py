0a9a49f28db95f63c461e78c81b3d39d3ff9c550
yjkwon1996 <yjkwon1996@gmail.com>
2021-11-29 21:00:14 +0900

    불량 사용자

--- bad_user.py
+++ bad_user.py
@@ -23,19 +23,37 @@ abd1** -> abc123 한가지 경우의 수
 # 순열 : 순서대로 뽑아서 줄을 세우는것(순서가 있다.)
 # 조합 : 무작위로 뽑아서 줄을 세우는것(순서가 없다.)
 # 써야되는것은 순서가 필요한 순열 permutation
+# 이후, 순열을 통해 구한 값과 banned_id의 값을 brute force로 비교한다.
 
-import itertools
+# brute force
+# 완전 탐색 알고리즘. 가능성 있는 모든 경우의 수를 탐색하면서 원하는 답을 찾는 방법.
+
+from itertools import permutations
+
+def check(users, banned_id) : 
+    for i in range(len(banned_id)) :
+        if len(users[i]) != len(banned_id[i]) : # user_id와 banned_id의 길이가 다르다면
+            return False                        # 그 user는 ban된 아이디가 아니다.
+        for j in range(len(users[i])) :
+            if banned_id[i][j] == '*' :         # banned_id의 글자가 *이면
+                continue                        # 다음 문자로 넘어가서 확인
+            if banned_id[i][j] != users[i][j] : # banned_id의 문자와 user_id의 문자가 다르다면
+                return False                    # 그 user는 ban된 아이디가 아니다.
+    return True
 
 def solution(user_id, banned_id):
-    answer = 0
+    user_per = list(permutations(user_id, len(banned_id)))
+    ban_set = []
     
+    for users in user_per :
+        if not check(users, banned_id) : # 각 순열을 통해 구한 값과 banned_id를 비교
+            continue
+        else :
+            users = set(users) # set으로 중복 제거
+            if users not in ban_set : # ban_set에 users가 없다면
+                ban_set.append(users) # ban_set에 추가
     
-    
-    return answer
-
-
-
-
+    return len(ban_set)
 
 
 if __name__ == "__main__" :
