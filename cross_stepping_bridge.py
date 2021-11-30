f06b5b95f1629f54e8c038ce7fda3108906b9a3f
yjkwon1996 <yjkwon1996@gmail.com>
2021-11-30 22:31:11 +0900

    징검다리 건너기

---
+++ cross_stepping_bridge.py
@@ -0,0 +1,18 @@
+# 2019 카카오 개발자 겨울 인턴십
+# 징검다리 건너기
+# https://programmers.co.kr/learn/courses/30/lessons/64062
+
+# 정확성, 효율성 테스트
+# 
+
+def solution(stones, k):
+    answer = 0
+    return answer
+
+
+if __name__ == "__main__" :
+    stones = [2, 4, 5, 3, 2, 1, 4, 2, 5, 1]
+    k = 3 
+    # result = 3
+    
+    print(solution(stones, k))
