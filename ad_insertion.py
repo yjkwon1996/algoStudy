5b7a9b31e784acb8bac9cc050fcb0fa999cb94e7
yjkwon1996 <yjkwon1996@gmail.com>
2022-01-11 19:24:27 +0900

    광고 삽입

---
+++ ad_insertion.py
@@ -0,0 +1,39 @@
+# 2021 KAKAO BLIND RECRUITMENT
+# 광고 삽입
+# https://programmers.co.kr/learn/courses/30/lessons/72414
+
+
+
+
+def solution(play_time, adv_time, logs):
+    answer = ''
+    
+    
+    
+
+
+    
+    return answer
+
+
+if __name__ == "__main__" :
+    play_time1 = "02:03:55"
+    adv_time1 = "00:14:15"
+    logs1 = ["01:20:15-01:45:14", "00:40:31-01:00:00", "00:25:50-00:48:29", "01:30:59-01:53:29", "01:37:44-02:02:30"]
+    # reseult = "01:30:59"
+    
+    play_time2 = "99:59:59"
+    adv_time2 = "25:00:00"
+    logs2 = ["69:59:59-89:59:59", "01:00:00-21:00:00", "79:59:59-99:59:59", "11:00:00-31:00:00"]
+    # result = "01:00:00"
+    
+    play_time3 = "50:00:00"
+    adv_time3 = "50:00:00"
+    logs3 = ["15:36:51-38:21:49", "10:14:18-15:36:51", "38:21:49-42:51:45"]
+    # result = "00:00:00"
+    
+    print(solution(play_time1, adv_time1, logs1))
+    print(solution(play_time2, adv_time2, logs2))
+    print(solution(play_time3, adv_time3, logs3))
+    
+    
