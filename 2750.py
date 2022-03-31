09dec98c38abe60b197c2a3fa53f7c685911b0a7
yjkwon1996 <yjkwon1996@gmail.com>
2022-03-31 20:26:37 +0900

    수 정렬하기

--- 2750.py
+++ 2750.py
@@ -99,9 +99,84 @@ for i in arr :
 # 10989 수 정렬하기 3
 # https://www.acmicpc.net/problem/10989
 
-# 위와 동일하지만 N의 범위가 10,000,000 이하
+# 위와 동일하지만 N의 범위가 10,000,000 이하. 중복 가능.
 # 수의 범위가 작다면 카운팅 정렬을 사용하여 더욱 빠르게 정렬할 수 있다
 # 카운팅 정렬(계수 정렬)
+# 메모리 초과
 
+"""
+N = int(input())
+arr = []
+for i in range(N) :
+    num = int(input())
+    arr.append(num) 
+cnt = [0] * (max(arr) + 1) # 숫자 i가 배열에 몇 개 존재하는지(중복 허용)
+
+for i in arr :
+    cnt[i] += 1
+
+for i in range(1, len(cnt)) : # 누적합 방식으로 갱신
+    cnt[i] += cnt[i-1]
+
+answer = [0] * len(arr) # arr의 각 원소를 정렬된 위치에 삽입
+
+# arr의 원소값을 cnt의 인덱스값으로 사용해 가져온 후 다시 answer의 인덱스 값으로 사용하여 arr의 원소로 저장
+# 이후, cnt[arr[i]]의 값 1 감소
+for i in arr :
+    idx = cnt[i]
+    answer[idx-1] = i
+    cnt[i] -= 1
+
+print(answer)
+
+
+"""
+
+# 딕셔너리를 이용해서도 가능한 카운팅 정렬
+
+"""
+
+N = int(input())
+arr = []
+for i in range(N) :
+    num = int(input())
+    arr.append(num) 
+
+cnt_dict = {}
+
+for i in arr :
+    if i in cnt_dict :
+        cnt_dict[i] += 1
+    else :
+        cnt_dict[i] = 1
+
+answer = []
+
+for i in range(max(arr) + 1) :
+    while i in cnt_dict and cnt_dict[i] != 0 :
+        answer.append(i)
+        cnt_dict[i] -= 1
+
+print(answer)
+    
+"""
+
+# 문제 풀이
+
+import sys
+
+N = int(sys.stdin.readline())
+arr = [0] * 10001
+
+# append가 아닌 다른 방식 이용. 미리 arr의 크기를 지정해준다(수 크기는 10000이하)
+# 각 배열의 인덱스 값을 이용하여 인덱스 값에서 몇 수만큼 있는지
+
+for i in range(N) :
+    arr[int(sys.stdin.readline())] += 1 
+    
+for i in range(10001) :
+    if arr[i] != 0 :
+        for j in range(arr[i]) :
+            print(i)
 
 
