3688c7c5b27456937004bcbffccef767acd33b41
yjkwon1996 <yjkwon1996@gmail.com>
2022-05-28 21:51:58 +0900

    1234 [S/W 문제해결 기본] 10일차 - 비밀번호

---
+++ samsung/1234.py
@@ -0,0 +1,94 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+# https://swexpertacademy.com/main/code/problem/problemDetail.do?problemLevel=3&contestProbId=AV14_DEKAJcCFAYD&categoryId=AV14_DEKAJcCFAYD&categoryType=CODE&problemTitle=&orderBy=RECOMMEND_COUNT&selectCodeLang=ALL&select-1=3&pageSize=10&pageIndex=3
+# 1234. [S/W 문제해결 기본] 10일차 - 비밀번호
+
+
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+# 표준 입력 예제
+'''
+a = int(input())                        정수형 변수 1개 입력 받는 예제
+b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제
+d = float(input())                      실수형 변수 1개 입력 받는 예제
+e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
+h = input()                             문자열 변수 1개 입력 받는 예제
+'''
+
+# 표준 출력 예제
+'''
+a, b = 6, 3
+c, d, e = 1.0, 2.5, 3.4
+f = "ABC"
+print(a)                                정수형 변수 1개 출력하는 예제
+print(b, end = " ")                     줄바꿈 하지 않고 정수형 변수와 공백을 출력하는 예제
+print(c, d, e)                          실수형 변수 3개 출력하는 예제
+print(f)                                문자열 1개 출력하는 예제
+'''
+
+# import sys
+
+
+'''
+      아래의 구문은 input.txt 를 read only 형식으로 연 후,
+      앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
+      여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
+      아래 구문을 이용하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
+
+      따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 구문을 사용하셔도 좋습니다.
+      아래 구문을 사용하기 위해서는 import sys가 필요합니다.
+
+      단, 채점을 위해 코드를 제출하실 때에는 반드시 아래 구문을 지우거나 주석 처리 하셔야 합니다.
+'''
+#sys.stdin = open("input.txt", "r")
+
+"""
+
+T = 10
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    num, arr = input().split()
+    arr = list(arr)
+
+    front = ''
+    idx = 0 # 인덱스용
+    while True :
+        if idx >= len(arr) : # arr을 끝까지 탐색했다면
+            break
+        if front == arr[idx] : # 바로 이전의 값과 동일하다면
+            arr.pop(idx) # 현재의 값 과
+            arr.pop(idx-1) # 이전의 값 제거
+            idx -= 2 # 이전의 idx로 가서 다시 확인
+        front = arr[idx]
+        idx += 1
+
+    print("#{}".format(test_case), ''.join(arr))
+
+"""
+
+
+# 이게 더 깔끔한듯
+T = 10
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+    num, arr = input().split()
+    num = int(num)
+    arr = list(arr)
+
+    front = ''
+    idx = 0 # 인덱스용
+    while True :
+        if idx == int(num) : # arr을 끝까지 탐색했다면
+            break
+        if front == arr[idx] : # 바로 이전의 값과 동일하다면
+            arr.pop(idx) # 현재의 값 과
+            arr.pop(idx-1) # 이전의 값 제거
+            idx -= 2 # 이전의 idx로 가서 다시 확인
+            num -= 2 # num값도 같이
+        front = arr[idx]
+        idx += 1
+
+    print("#{}".format(test_case), ''.join(arr))
