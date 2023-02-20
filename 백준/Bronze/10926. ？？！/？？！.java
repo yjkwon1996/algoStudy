16b12eccea53fe0d9c116f0978499ff733107f1e
YongJin Kwon <66740126+yjkwon1996@users.noreply.github.com>
2023-02-20 09:25:38 +0900

    [Bronze V] Title: ??!, Time: 80 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// https://www.acmicpc.net/problem/24479
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(br.readLine() + "??!");
+    }
+}
