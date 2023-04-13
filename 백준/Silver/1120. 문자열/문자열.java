f0677ad3cb4400638fe3517223b87a463eca9bca
YongJin Kwon <66740126+yjkwon1996@users.noreply.github.com>
2023-04-13 23:02:24 +0900

    [Silver IV] Title: 문자열, Time: 76 ms, Memory: 11492 KB -BaekjoonHub

---
+++ 백준/Silver/1120. 문자열/문자열.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String A = st.nextToken();
+        String B = st.nextToken();
+
+        int res = A.length();
+        for (int i=0; i<=B.length()-A.length(); i++) {
+            int cnt = 0;
+            for (int j=0; j<A.length(); j++) {
+                if (A.charAt(j) != B.charAt(i+j)) {
+                    cnt++;
+                }
+            }
+            res = Math.min(res, cnt);
+        }
+        System.out.println(res);
+    }
+}
