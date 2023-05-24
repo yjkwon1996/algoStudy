8187365295909b9a40c7cb953e977e1070135403
YongJin Kwon <66740126+yjkwon1996@users.noreply.github.com>
2023-05-24 16:12:30 +0900

    [level 3] Title: 거스름돈, Time: 901.52 ms, Memory: 12 MB -BaekjoonHub

--- 프로그래머스/lv3/12907. 거스름돈/거스름돈.py
+++ 프로그래머스/lv3/12907. 거스름돈/거스름돈.py
@@ -6,10 +6,9 @@ def solution(n, money):
     dp = [0 for i in range(n+1)] # dp[i]는 i원을 만들 수 있는 경우의 수
     dp[0] = 1 # 초기값. 0원을 만들기 위한 경우의 수는 1개
     
-    for value in money :
-        for unit in range(value, n+1) :
+    for value in money : # 동전 단위 value
+        for unit in range(value, n+1) : # 현재 금액을 만들기 위해 동전을 사용하는 
             dp[unit] = ( dp[unit] + dp[unit-value]) % 1000000007
-    
     answer = dp[-1]
     
     return answer
